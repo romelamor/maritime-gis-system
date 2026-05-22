4d305e50d8ea9de810e28c547f03ee76543e2ad0
Romel Amor <romelamor55@gmail.com>
2026-05-22 09:35:33 +0800

    Changed Gmail SMTP to SSL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -201,7 +201,7 @@ FRONTEND_LOGIN_URL = config(
 # ==========================
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT =  465
 EMAIL_USE_TLS = True
 
 # IMPORTANT — Replace these
